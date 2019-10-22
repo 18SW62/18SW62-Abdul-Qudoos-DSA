be32e319340ed589d2976daa97af57bd4dc29ded
18SW62 <49367648+18SW62@users.noreply.github.com>
2019-10-22 21:07:40 +0500

    Add files via upload

--- lab1/Task1/task1.java
+++ lab1/Task1/task1.java
@@ -1,15 +1,15 @@
 class Example{
-	int salary,hours; //declear a instants varaibles;
+	int salary,hours; //declearment of instants varaibles;
 	public void getinfo(int salary,int hours){
 		this.salary=salary;
 		this.hours=hours;
 	}
-	public void addSal(){//method for increase in salary upto 10 if it is less than 500;
+	public void addSal(){//method for increase in salary upto 10 if it is less than 500 dollars;
 		if(salary<500){
 		salary+=10;
 	}
 }
-	public void addwork(){//method for increase in salary upto 5 if it is less than 500;
+	public void addwork(){//method for increase in salary upto 5 if it is greater than 6 hours;
 		if(hours>6){
 		salary+=5;
 		}
